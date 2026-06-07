fa142d45401ffc65af6d30ecc2eaeb2ce4a564af
Aryan Dev Shourie <shouriearyandev@gmail.com>
2026-06-07 17:01:35 +0530

    added localhost fallback for callbackURL in passport.js

--- streamforge-api/src/config/passport.js
+++ streamforge-api/src/config/passport.js
@@ -12,7 +12,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: process.env.GOOGLE_CALLBACK_URL
+            callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:5001/auth/google/callback"
         },
         async (
             accessToken,
